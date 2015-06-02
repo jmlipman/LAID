0e923d165123b11c307aa5ce9083826d0f2e4aff
Juan Miguel Valverde Martínez <regjmlipman@hotmail.com>
2015-06-02 23:37:56 +0200

    Create activation.m

---
+++ NN/activation.m
@@ -0,0 +1,26 @@
+% ACTIVATION.m - Activation function
+% 
+% This code is the activation function of a neural network. Given a certain
+% input and the type of activation function, it will decide an output
+%
+% You can do with this code whatever you want. The main purpose is help
+% people learning about this. Also, there is no warranty of any kind.
+%
+% Juan Miguel Valverde Martinez
+% http://laid.delanover.com
+%
+% @param inout: input to be treated.
+% @param type: type of activation function (step or sigmoid).
+% @return output: output.
+
+function [ output ] = activation( input, type )
+    if strcmp(type,'step')
+       if(input>0)
+           output=1;
+       else
+           output=0;
+       end
+    elseif strcmp(type,'sigmoid')
+        output=1/(1+exp(-input));
+    end
+end
